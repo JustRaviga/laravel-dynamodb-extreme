ac8de4c7797739db117fddcb4dd5528b29d06136
Rob <robw@raviga.co.uk>
2023-06-07 16:40:14 +0100

    Fix syntax error

--- src/Exceptions/InvalidInlineModel.php
+++ src/Exceptions/InvalidInlineModel.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace JustRaviga\DynamoDb\Exceptions;
 
 use JustRaviga\DynamoDb\Models\DynamoDbModel;
+use RuntimeException;
 
-final class InvalidInlineModel extends \RuntimeException
+final class InvalidInlineModel extends RuntimeException
 {
     public function __construct(DynamoDbModel $model)
     {
-        parent::__construct("Invalid inline model {{$model::class}}");
+        $class = $model::class;
+        parent::__construct("Invalid inline model {{$class}}");
     }
 }
